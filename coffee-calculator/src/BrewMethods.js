060d54d42e8f2520029e82aaf20bddd144f78d8d
Tyler Davis <datadazer@gmail.com>
2018-05-25 17:12:20 -0600

    Setup React and start separating out components

---
+++ coffee-calculator/src/BrewMethods.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class BrewMethods extends Component {
+  selectBrewMethod() {
+    // do stuff
+  }
+  render() {
+    return (
+      <div className="row justify-content-around">
+        <div className="col-xs-2 m-4">
+          <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1627/2455/t/1/assets/icon-brewguide-aeropress.svg"></img>
+        </div>
+        <div className="col-xs-2 m-4">
+          <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1627/2455/t/1/assets/icon-brewguide-chemex.svg"></img>
+        </div>
+        <div className="col-xs-2 m-4">
+          <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1627/2455/t/1/assets/icon-brewguide-pourover.svg"></img>
+        </div>
+        <div className="col-xs-2 m-4">
+          <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1627/2455/t/1/assets/icon-brewguide-frenchpress.svg"></img>
+        </div>
+        <div className="col-xs-2 m-4">
+          <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1627/2455/t/1/assets/icon-brewguide-drip.svg"></img>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BrewMethods;
