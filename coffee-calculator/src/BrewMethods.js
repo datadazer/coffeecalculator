e36f5f9571b7eb90b8df0ba3e5086095d561ffc4
Tyler Davis <datadazer@gmail.com>
2018-06-10 16:10:48 -0600

    change component class to stateless functional component

--- coffee-calculator/src/BrewMethods.js
+++ coffee-calculator/src/BrewMethods.js
@@ -2,31 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class BrewMethods extends Component {
-  selectBrewMethod() {
+const BrewMethods = () => {
+  const selectBrewMethod = () => {
     // do stuff
   }
-  render() {
-    return (
-      <div className="row justify-content-around">
-        <div className="col-xs-2 m-4">
-          <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1627/2455/t/1/assets/icon-brewguide-aeropress.svg"></img>
-        </div>
-        <div className="col-xs-2 m-4">
-          <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1627/2455/t/1/assets/icon-brewguide-chemex.svg"></img>
-        </div>
-        <div className="col-xs-2 m-4">
-          <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1627/2455/t/1/assets/icon-brewguide-pourover.svg"></img>
-        </div>
-        <div className="col-xs-2 m-4">
-          <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1627/2455/t/1/assets/icon-brewguide-frenchpress.svg"></img>
-        </div>
-        <div className="col-xs-2 m-4">
-          <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1627/2455/t/1/assets/icon-brewguide-drip.svg"></img>
-        </div>
+  return (
+    <div className="row justify-content-around">
+      <div className="col-xs-2 m-4">
+        <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1627/2455/t/1/assets/icon-brewguide-aeropress.svg"></img>
       </div>
-    );
-  }
+      <div className="col-xs-2 m-4">
+        <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1627/2455/t/1/assets/icon-brewguide-chemex.svg"></img>
+      </div>
+      <div className="col-xs-2 m-4">
+        <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1627/2455/t/1/assets/icon-brewguide-pourover.svg"></img>
+      </div>
+      <div className="col-xs-2 m-4">
+        <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1627/2455/t/1/assets/icon-brewguide-frenchpress.svg"></img>
+      </div>
+      <div className="col-xs-2 m-4">
+        <img className="card-img-top" src="https://cdn.shopify.com/s/files/1/1627/2455/t/1/assets/icon-brewguide-drip.svg"></img>
+      </div>
+    </div>
+  );
 }
 
 export default BrewMethods;
